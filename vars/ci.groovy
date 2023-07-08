73ae0a617475b99844935993a04a483afad8ef5a
akhil <akhileshreddy9181@gmail.com>
2023-07-08 12:39:37 -0400

    Added Exit Status to script

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -7,11 +7,13 @@ def call () {
 
             stage('Compile/Build') {
                 steps {
-                    if(app_lang == "nodejs") {
-                        sh 'npm install'
-                    }
-                    if(app_lang == "maven") {
-                        sh 'mvn package'
+                    script {
+                        if(app_lang == "nodejs") {
+                            sh 'npm install'
+                        }
+                        if(app_lang == "maven") {
+                            sh 'mvn package'
+                        }
                     }
                 }
             }
