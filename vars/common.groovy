21e84f4f8d4e7beb388f45fc15965317cdad8c34
akhil <akhileshreddy9181@gmail.com>
2023-07-10 16:10:41 -0400

    Added Exit Status to script

--- vars/common.groovy
+++ vars/common.groovy
@@ -27,9 +27,10 @@ def codequality() {
 
 def prepareArtifacts() {
     sh 'echo ${TAG_NAME} >VERSION'
-    if (app_lang == "nodejs")
+    if (app_lang == "nodejs" || app_lang == "angular")
     {
-        sh 'zip -r ${component}-${TAG_NAME}.zip server.js node_modules VERSION'
+        sh 'zip -r ${component}-${TAG_NAME}.zip * -x Jenkinsfile'
     }
+
 
 }
