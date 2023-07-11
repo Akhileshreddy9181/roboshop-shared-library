f54b333877a6efba1d50b93f7c2bf245aac458b8
akhil <akhileshreddy9181@gmail.com>
2023-07-10 21:01:42 -0400

    Added Exit Status to script

--- vars/common.groovy
+++ vars/common.groovy
@@ -30,10 +30,10 @@ def prepareArtifacts() {
 
     if(app_lang == "maven")
     {
-        sh 'zip -r ${component}-${TAG_NAME}.zip ${component}.jar VERSION'
+        sh 'zip -r ${component}-${TAG_NAME}.zip ${component}.jar schema VERSION'
     }
     else {
-        sh 'zip -r ${component}-${TAG_NAME}.zip * -x Jenkinsfile VERSION'
+        sh 'zip -r ${component}-${TAG_NAME}.zip * -x Jenkinsfile'
     }
 
 }
