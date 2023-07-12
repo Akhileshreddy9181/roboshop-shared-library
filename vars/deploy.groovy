a8f2b2939d1dee7ce9966cda84d02183cb5c128c
akhil <akhileshreddy9181@gmail.com>
2023-07-11 20:58:58 -0400

    Added Exit Status to script

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -2,6 +2,10 @@ def call () {
     pipeline {
         agent any
 
+
+        options {
+            ansiColor('xterm')
+        }
         parameters {
             string(name: 'app_version', defaultValue: '', description: 'App Version')
             string(name: 'component', defaultValue: '', description: 'Component')
@@ -18,9 +22,16 @@ def call () {
 
             stage('Deploy Servers') {
                 steps {
-                    sh 'aws ec2 describe-instances --filters "Name=tag:Name,Values=${component}-${environment}" -- query "Reservations[*].Instances[*].PrivateIpAddress" --output text >/tmp/servers'
+                    env.SSH_PASSWORD = sh (script: ' aws ssm get-parameter --name prod.ssh.pass --with-decryption | jq .Parameter.Value | xargs', returnStdout: true).trim()
 
-                    sh 'ansible-playbook -i /tmp/servers roboshop.yml -e role_name=${component} -e env=${environment}'
+
+                    wrap([$class: 'MaskPasswordsBuildWrapper',
+                          varPasswordPairs: [[password: SSH_PASSWORD]]]) {
+                        sh 'aws ec2 describe-instances --filters "Name=tag:Name,Values=${component}-${environment}" -- query "Reservations[*].Instances[*].PrivateIpAddress" --output text >/tmp/servers'
+
+                        sh 'ansible-playbook -i /tmp/servers roboshop.yml -e role_name=${component} -e env=${environment} -e ansible_user=centos -e env.ansible_password=${SSH_PASSWORD}'
+
+                    }
 
                 }
             }
