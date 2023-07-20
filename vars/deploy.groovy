bd8083668e4a6276a1fa04676c9f485d9fb495c9
akhil <akhileshreddy9181@gmail.com>
2023-07-20 19:57:30 -0400

    Added Exit Status to script

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -13,20 +13,30 @@ def call () {
         }
 
         stages {
-            stage('Clone Application') {
+            stage('Update Parameter Store') {
                 steps {
-                    dir('APP') {
-                        git branch: 'main', url: "https://github.com/akhileshreddy9181/${component}"
-                    }
+                    sh 'aws ssm put-parameter --name ${environment}.${component}.app_version --type "String" --value "${app_version}" --overwrite'
 
                 }
             }
 
-            stage('Deploy Helm Chart') {
+            stage('Deploy Servers') {
                 steps {
                     script {
-                        sh 'helm install ${component} APP/helm/${environment}.yaml'
 
+                        env.SSH_PASSWORD = sh (script: 'aws ssm get-parameter --name prod.ssh.pass --with-decryption | jq .Parameter.Value | xargs', returnStdout: true).trim()
+
+                        /*echo 'Printing SSH password'
+                        echo "$SSH_PASSWORD"*/
+
+
+                        wrap([$class: 'MaskPasswordsBuildWrapper',
+                              varPasswordPairs: [[password: SSH_PASSWORD]]]) {
+                            sh 'aws ec2 describe-instances --filters "Name=tag:Name,Values=${component}-${environment}" --query "Reservations[*].Instances[*].PrivateIpAddress" --output text |xargs -n1>/tmp/servers'
+
+                            sh 'ansible-playbook -i /tmp/servers roboshop.yml -e role_name=${component} -e env=${environment} -e ansible_user=centos -e ansible_password=${SSH_PASSWORD}'
+
+                        }
                     }
 
                 }
